f5f9eabba41657b93b3d1adc5bf9b8dbc6b80ada
lincate <lincater@gmail.com>
2019-04-17 22:16:37 +0800

    Project initial

---
+++ core/src/main/java/com/jovo/keeper/KeeperApplication.java
@@ -0,0 +1,21 @@
+package com.jovo.keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class KeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeeperApplication.class, args);
+    }
+
+    @RequestMapping("/name")
+    public String myName(){
+        return "jovo";
+    }
+
+}
